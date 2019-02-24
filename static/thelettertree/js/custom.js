421dc585423cc66a3c7046c7a5393a5dfdc70b6d
Luke Smyth <smythluke123@gmail.com>
2019-02-24 20:38:34 +0000

    Update
    updates to all sections of the site.

--- static/thelettertree/js/custom.js
+++ static/thelettertree/js/custom.js
@@ -1,12 +1,88 @@
 $(function () {
+
+	// change colour of nav bar as your scroll down
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > window.innerHeight-100) {
+		if ($(this).scrollTop() > window.innerHeight - 350) {
 			$(".navbar").removeClass("bg-transparent")
 			$(".navbar").addClass("bg-primary");
-		}
-		if ($(this).scrollTop() < window.innerHeight-100) {
+			$(".navbar").addClass("border-bottom");
+		} else {
 			$(".navbar").removeClass("bg-primary")
+			$(".navbar").removeClass("border-bottom")
 			$(".navbar").addClass("bg-transparent")
 		}
+		var parallax = (Math.min(50+($(document).scrollTop()/$("#titlePage").height()*50), 100)) + "%";
+		$("#titlePage").css("background-position-y", parallax);
 	});
+
+	// scrollspy
+	$('body').scrollspy({ target: "#navbar-scroll", offset: $(".navbar").outerHeight()})
+
+	// open up the photoswipe gallery when you click on an image
+	$("#gallery img").click(function () {
+		var pswpElement = $(".pswp")[0];
+		var pageYScroll = window.pageYOffset || document.documentElement.scrollTop
+		var items = [];
+
+		$("#gallery img").each(function (i) {
+			thumbnailWidth = this.naturalWidth;
+			thumbnailHeight = this.naturalHeight;
+			imageName = this.src.split("/").pop();
+			largeSrc = images[imageName].large.file;
+			var img = {};
+			img.msrc = this.src;
+			img.src = largeSrc;
+			img.w = images[imageName].large.width;
+			img.h = images[imageName].large.height;
+			img.thumbnail = { "x": this.x, "y": this.y + pageYScroll, "w": this.width };
+			items.push(img);
+		});
+
+		var options = {
+			"index": $(this).parent().index(),
+			"showHideOpacity": true,
+			"bgOpacity": 0.6,
+			"history": false,
+			"barsSize": { "top": 64, "bottom": "auto" },
+			"shareEl": false,
+			"fullscreenEl": false,
+			"zoomEl": false,
+			getThumbBoundsFn: function (index) {
+				return items[index].thumbnail;
+			}
+		};
+
+		var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+		gallery.init();
+	});
+
+	// close nav menu on mobile after clicking link
+	$('.navbar a').on('click', function () {
+		$('.navbar-collapse').removeClass('show');
+	})
+
+	// scroll the page when you click on an A tag
+	$("a").click(
+		function (event) {
+			if (this.hash !== "") {
+				event.preventDefault();
+				var hash = this.hash;
+				$("html, body").animate({
+					scrollTop: $(hash).offset().top-$(".navbar").outerHeight()
+				},
+					800
+				);
+			}
+		}
+	);
+
+	// var myPath = $("#path2362")[0];
+    // var segment = new Segment(myPath);
+	// segment.draw("0%", "100%", 5);
+
+
+
+
+
+
 });
---
+++ static/thelettertree/js/data.js
@@ -0,0 +1,98 @@
+var images = {
+	"image1.png": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image1.png",
+			width: 480,
+			height: 270
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image1.png",
+			width: 1920,
+			height: 1080
+		}
+	},
+	"image1.jpeg": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image1.jpeg",
+			width: 400,
+			height: 324
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image1.jpeg",
+			width: 1600,
+			height: 1296
+		}
+	},
+	"image2.png": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image2.png",
+			width: 500,
+			height: 375
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image2.png",
+			width: 2000,
+			height: 1500
+		}
+	},
+	"image2.jpg": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image2.jpg",
+			width: 325,
+			height: 216
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image2.jpg",
+			width: 1300,
+			height: 864
+		}
+	},
+	"image3.png": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image3.png",
+			width: 375,
+			height: 500
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image3.png",
+			width: 1500,
+			height: 2000
+		}
+	},
+	"image3.jpg": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image3.jpg",
+			width: 250,
+			height: 250
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image3.jpg",
+			width: 1000,
+			height: 1000
+		}
+	},
+	"image4.png": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image4.png",
+			width: 270,
+			height: 480
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image4.png",
+			width: 1080,
+			height: 1920
+		}
+	},
+	"image4.jpg": {
+		small: {
+			file: "static/thelettertree/img/gallery/small/image4.jpg",
+			width: 417,
+			height: 278
+		},
+		large: {
+			file: "static/thelettertree/img/gallery/large/image4.jpg",
+			width: 1668,
+			height: 1112
+		}
+	}
+}
